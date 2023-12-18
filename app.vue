24c141b1cf05e979a6e09141dc47dbc79c5656ac
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2023-12-18 23:44:59 +0600

    first

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLoadingIndicator />
+  <Header />
+
+  <main class="container">
+    <NuxtPage />
+  </main>
+</template>
+
+<script setup>
+import("~/assets/css/pico.min.css");
+
+useHead({
+  htmlAttrs: {
+    "data-theme": "light",
+  },
+});
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <nav>
+      <ul>
+        <li><strong>Brand</strong></li>
+      </ul>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="contact">Contact</NuxtLink></li>
+        <li><NuxtLink to="about">About</NuxtLink></li>
+      </ul>
+    </nav>
+  </div>
+</template>
